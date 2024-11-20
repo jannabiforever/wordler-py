733d3d5bc72a1a4f63659cc03bf9020846d740f4
jannabiforever <jungini1226@gmail.com>
2024-11-20 22:39:27 +0900

    requirements

--- wordler/dictionary.py
+++ wordler/dictionary.py
@@ -1,17 +1,8 @@
-from typing import List, Tuple
-
-
-def parse_down_each_line(lines: List[str]) -> List[Tuple[str, int]]:
-    result = []
-    for line in lines:
-        word, freq = line.split()
-        result.append((word, int(freq)))
-
-    return result
+from typing import List
 
 
 def get_words_list() -> List[str]:
     def get_word(x): return x[0]
     with open("dictionary.txt") as f:
-        parsed = parse_down_each_line(f.read().splitlines())
+        parsed = f.read().splitlines()
         return list(map(get_word, parsed))
