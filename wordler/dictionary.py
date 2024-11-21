878a96860c3828a226307f5db4f8e3fdf96b9603
jannabiforever <jungini1226@gmail.com>
2024-11-21 13:44:36 +0900

    unnecessary print debug

--- wordler/dictionary.py
+++ wordler/dictionary.py
@@ -4,7 +4,3 @@ from typing import List
 def get_words_list() -> List[str]:
     with open("dictionary.txt") as f:
         return f.read().splitlines()
-
-
-if __name__ == "__main__":
-    print(get_words_list())
